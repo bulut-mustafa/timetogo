2152d4200fd609bbe41b46573a26c7aa3fcb1012
Mustafa Bulut <mustafaiste@outlook.com>
2025-02-28 14:31:24 +0800

    fix api call

--- app/api/unsubscribe/route.ts
+++ app/api/unsubscribe/route.ts
@@ -1,10 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { doc, deleteDoc , getDoc} from "firebase/firestore";
 import { db } from "@/firebase";
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const { reservationId, destination } = req.query;
 
